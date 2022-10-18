2b63be867694e4e9b693f1388e1ccd6e5a52827a
Natanael <natanael_magalhaes@msn.com>
2022-10-18 05:26:36 -0300

    xtable

---
+++ pages/xtable.tsx
@@ -0,0 +1,26 @@
+import { Container } from 'react-bootstrap'
+import AppNavbar from '../components/common/AppNavbar'
+import LoadInitialState from '../components/xtable/LoadInitialState'
+import ConfigForm from '../components/xtable/ConfigForm'
+import { XTableConfigProvider } from '../contexts/XTableConfigContext'
+import XTableBody from '../components/xtable/XTableBody'
+
+export default function XTable() {
+  return (
+    <>
+      <XTableConfigProvider>
+        <LoadInitialState />
+
+        <AppNavbar />
+
+        <Container className="mt-3">
+          <ConfigForm />
+        </Container>
+
+        <Container fluid className="px-0">
+          <XTableBody />
+        </Container>
+      </XTableConfigProvider>
+    </>
+  )
+}
