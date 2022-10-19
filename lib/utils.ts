e31e02c565e6568b136efd58bd8c2c09cbe977db
Natanael <natanael_magalhaes@msn.com>
2022-10-19 14:06:27 -0300

    utilities

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,19 @@ export function setUrlParams(url: string, params: { [key: string]: string }) {
   }
   return u.toString()
 }
+
+export function obj2formData(obj: { [key: string]: any }) {
+  const fd = new FormData()
+  for (const key in obj) {
+    fd.append(key, obj[key])
+  }
+  return fd
+}
+
+export function obj2qs(obj: { [key: string]: any }) {
+  return Object.entries(obj).map(([k, v]) => `${k}=${v}`).join('&')
+}
+
+export function objPosId(ra: number, dec: number) {
+  return [Math.round(ra * 1e10), Math.round(dec * 1e10)]
+}
