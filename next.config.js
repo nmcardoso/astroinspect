9ff081bdf6c2e290344e55ed7405e7c03d45071e
Natanael <natanael_magalhaes@msn.com>
2022-10-20 05:11:46 -0300

    add basepath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NODE_ENV === 'production' ? '/astrotools' : '/'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  basePath
 }
 
 module.exports = nextConfig
