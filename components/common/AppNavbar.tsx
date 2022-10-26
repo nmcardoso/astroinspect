4b12cd6be8f7109d0c8f7902a7cf549d8d5cd8d9
Natanael <natanael_magalhaes@msn.com>
2022-10-25 21:16:34 -0300

    citation button

--- components/common/AppNavbar.tsx
+++ components/common/AppNavbar.tsx
@@ -3,9 +3,89 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import { useEffect, useState } from 'react'
+import Badge from 'react-bootstrap/Badge'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+
+const DOI = '10.5281/zenodo.7237378'
+const CITATION = `@article{astrotools,
+    doi = {10.5281/zenodo.7237378},
+    url = {https://doi.org/10.5281/zenodo.7237378},
+    year = {2022},
+    month = oct,
+    publisher = {Zenodo},
+    author = {Natanael M. Cardoso},
+    title = {Astrotools: Web-based astronomical tools},
+    journal = {Zenodo}
+}`
+
+
+
+const CitationModal = ({ show, onHide }: any) => {
+  const [isDoiCopied, setDoiCopied] = useState(false)
+  const [isCitationCopied, setCitationCopied] = useState(false)
+
+  useEffect(() => {
+    if (isDoiCopied) setTimeout(() => setDoiCopied(false), 3000)
+    if (isCitationCopied) setTimeout(() => setCitationCopied(false), 3000)
+  }, [isDoiCopied, isCitationCopied])
+
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      animation={false}
+      size="lg"
+      centered>
+      <Modal.Header closeButton>
+        <Modal.Title as="h5" id="contained-modal-title-vcenter">
+          How to cite this software
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          If you used this software during the production process of
+          your scientific project, please consider citing it with
+          the information provided below.
+        </p>
+
+        <div className="mt-4">
+          <Badge>DOI</Badge>
+          <CopyToClipboard
+            text={DOI}
+            onCopy={() => setDoiCopied(true)}>
+            <Button variant="link" size="sm">Copy to Clipboard</Button>
+          </CopyToClipboard>
+          {isDoiCopied && <small className="text-success ms-3">Copied!</small>}
+          <pre className="pre-scrollabes bg-dark bg-opacity-10 rounded-3 p-3">
+            {DOI}
+          </pre>
+        </div>
+
+        <div className="mt-4">
+          <Badge>BibTex</Badge>
+          <CopyToClipboard
+            text={CITATION}
+            onCopy={() => setCitationCopied(true)}>
+            <Button variant="link" size="sm">Copy to Clipboard</Button>
+          </CopyToClipboard>
+          {isCitationCopied && <small className="text-success ms-3">Copied!</small>}
+          <pre className="pre-scrollabes bg-dark bg-opacity-10 rounded-3 p-3">
+            {CITATION}
+          </pre>
+        </div>
+      </Modal.Body>
+    </Modal>
+  )
+}
 
 
 export default function AppNavbar() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <Navbar bg="light" expand="lg" className="border-bottom">
       <Container>
@@ -20,6 +100,23 @@ export default function AppNavbar() {
             <span className="ms-2">AstroTools</span>
           </div>
         </Navbar.Brand>
+
+        <div className="mx-auto">
+          <span className="fw-bold">XTable</span>
+        </div>
+
+        <div>
+          <Button
+            size="sm"
+            variant="outline-primary"
+            onClick={() => setShowModal(true)}>
+            Cite this software
+          </Button>
+
+          <CitationModal
+            show={showModal}
+            onHide={() => setShowModal(false)} />
+        </div>
         {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
