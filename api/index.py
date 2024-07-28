9954c74e683006b05dec75310133a3585ee40aa8
Natanael <natanael_magalhaes@msn.com>
2024-07-28 03:28:54 -0300

    fix

--- api/index.py
+++ api/index.py
@@ -432,12 +432,11 @@
 
 
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import html, json
 
-app = Sanic()
+app = Sanic('app')
  
  
-@app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
-    return json({'hello': path})
+    return html('hello ' + path)
