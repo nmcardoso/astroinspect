5d8cfe3258a250502be6faf8c60475aa372adb03
Natanael <natanael_magalhaes@msn.com>
2024-07-28 22:24:56 -0300

    update version

--- contexts/XTableConfigContext.tsx
+++ contexts/XTableConfigContext.tsx
@@ -108,7 +108,7 @@ export interface IState {
   customImaging: ICustomImaging,
 }
 
-export const SCHEMA_VERSION: number = 9
+export const SCHEMA_VERSION: number = 10
 
 const getInitialState = (): IState => ({
   schemaVersion: SCHEMA_VERSION,
