e566ae3630235ad2345834f03a3dc3dd0a4707c6
Natanael <natanael_magalhaes@msn.com>
2024-09-26 01:51:27 -0300

    generate column definitions for ag-grid

--- app/lib/TableHelper.ts
+++ app/lib/TableHelper.ts
@@ -1,5 +1,11 @@
-import { IState } from '../contexts/XTableDataContext'
 import Papa, { ParseResult } from 'papaparse'
+import { useXTableConfig, IState } from '@/contexts/XTableConfigContext'
+import ClassCell from '@/components/table/ClassCell'
+import { ColDef } from '@ag-grid-community/core'
+import imageCellFactory from '@/components/table/ImageCell'
+import { findIndex } from './utils'
+import { queuedState } from './states'
+import AsyncTextCell from '@/components/table/AsyncTextCell'
 
 
 interface ITableSummary {
@@ -10,13 +16,82 @@ interface ITableSummary {
 }
 
 
-const findIndex = (query: string, items: string[]) => {
-  const pattern = `^${query}[_\d]*?$`
-  const regex = RegExp(pattern, 'gi')
-  return items.findIndex((item, i) => {
-    return regex.exec(item) !== null
-  })
+const idColDef: ColDef = {
+  field: 'ai:id',
+  maxWidth: 65,
+  headerName: '#'
 }
+
+const classificationColDef: ColDef = {
+  field: 'ai:class',
+  flex: 1,
+  headerName: 'class',
+  cellRenderer: ClassCell
+}
+
+const splusImagingColDef: ColDef = {
+  field: 'img:splus',
+  flex: 1,
+  headerName: 'splus',
+  cellRenderer: imageCellFactory({zoomHeight: 650, zoomWidth: 650, modalSize: 'lg'}),
+}
+
+const legacyImagingColDef: ColDef = {
+  field: 'img:legacy',
+  flex: 1,
+  headerName: 'legacy',
+  cellRenderer: imageCellFactory({zoomHeight: 650, zoomWidth: 650, modalSize: 'lg'}),
+}
+
+const sdssSpectraColDef: ColDef = {
+  field: 'img:sdss_spec',
+  flex: 1,
+  headerName: 'spec',
+  cellRenderer: imageCellFactory({zoomHeight: 650, zoomWidth: 650, modalSize: 'lg'}),
+}
+
+const splusPhotoSpectraColDef: ColDef = {
+  field: 'img:splus_photospec',
+  flex: 1,
+  headerName: 'photo spec',
+  cellRenderer: imageCellFactory({zoomHeight: 625, modalSize: 'lg'}),
+}
+
+const nearbyRedshiftsColDef: ColDef = {
+  field: 'ai:nearby_redshifts',
+  flex: 1,
+  headerName: 'nearby z'
+}
+
+const customImagingColDefFactory = (id: number): ColDef => {
+  return {
+    field: `img:custom_${id}`,
+    flex: 1,
+    headerName: `custom ${id}`,
+    cellRenderer: imageCellFactory({zoomHeight: 650, zoomWidth: 650, modalSize: 'lg'}),
+  }
+}
+
+const sdssCatalogColDefFactory = (table: string, col: string): ColDef => {
+  return {
+    field: `sdss:${table}.${col}`,
+    flex: 1,
+    headerName: col.toLowerCase(),
+    cellRenderer: AsyncTextCell,
+  }
+}
+
+const userTableColDefFactory = (colName: string): ColDef => {
+  return {
+    field: `tab:${colName}`,
+    // field: colName,
+    flex: 1,
+    headerName: colName.toLowerCase(),
+  }
+}
+
+
+
 
 class TableHelper {
   constructor() { }
@@ -67,24 +142,66 @@ class TableHelper {
     })
   }
 
-  getRa(rowIndex: number, tableData: IState): number | string | null {
-    if (tableData.raIndex != null && rowIndex < tableData.data.length) {
-      return tableData.sourceData[rowIndex + 1][tableData.raIndex]
+  getColDefs(tcState: IState): {colDef: ColDef[], initVal: any} {
+    const defs: ColDef[] = [idColDef]
+    const initVal: any = {}
+    
+    // Classification
+    if (tcState.classification.enabled) {
+      defs.push(classificationColDef)
+      initVal['ai:class'] = undefined
     }
-    return null
-  }
 
-  getDec(rowIndex: number, tableData: IState): number | string | null {
-    if (tableData.decIndex != null && rowIndex < tableData.data.length) {
-      return tableData.sourceData[rowIndex + 1][tableData.decIndex]
+    // User Table
+    if (!!tcState.table.selectedColumnsId) {
+      for (const colId of tcState.table.selectedColumnsId) {
+        const colName = tcState.table.columns[colId]
+        defs.push(userTableColDefFactory(colName))
+      }
     }
-    return null
-  }
 
-  getCellValue(rowId: number, colId: string, tableData: IState) {
-    return tableData.data?.[rowId]?.[colId]
+    // SDSS Catalog
+    if (!!tcState.sdssCatalog.selectedColumns) {
+      for (const col of tcState.sdssCatalog.selectedColumns) {
+        defs.push(sdssCatalogColDefFactory(col.table, col.column))
+        initVal[`sdss:${col.table}.${col.column}`] = queuedState
+      }
+    }
+
+    // SDSS Spectra
+    if (tcState.sdssSpectra.enabled) {
+      defs.push(sdssSpectraColDef)
+      initVal['img:sdss_spec'] = queuedState
+    }
+
+    // S-PLUS Photo Spectra
+    if (tcState.splusPhotoSpectra.enabled) {
+      defs.push(splusPhotoSpectraColDef)
+      initVal['img:splus_photospec'] = queuedState
+    }
+
+    // Legacy Stamp
+    if (tcState.legacyImaging.enabled) {
+      defs.push(legacyImagingColDef)
+      initVal['img:legacy'] = queuedState
+    }
+
+    // S-PLUS Stamp
+    if (tcState.splusImaging.enabled) {
+      defs.push(splusImagingColDef)
+      initVal['img:splus'] = queuedState
+    }
+
+    // Custom Imaging
+    if (tcState.customImaging.enabled) {
+      for (const col of tcState.customImaging.columns) {
+        defs.push(customImagingColDefFactory(col.columnIndex))
+        initVal[`img:custom_${col.columnIndex}`] = queuedState
+      }
+    }
+
+    return {colDef: defs, initVal: initVal}
   }
 }
-
 
 export default new TableHelper()
