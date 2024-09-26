7f13e0fa7bb08ab86f1a7f62923c01c5b6c0de39
Natanael <natanael_magalhaes@msn.com>
2024-09-26 20:09:20 -0300

    main app

---
+++ app/components/common/AstroInspect.tsx
@@ -0,0 +1,49 @@
+import { Button, Container } from 'react-bootstrap'
+import AppNavbar from '@/components/common/AppNavbar'
+import ConfigForm from '@/components/setup/ConfigForm'
+import XTableBody from '@/components/table/_XTableBody'
+import { XTableDataProvider } from '@/contexts/XTableDataContext'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import Emitter from '@/lib/Emitter'
+import { FaArrowLeft } from "react-icons/fa";
+import DownloadTableButton from './DownloadTableButton'
+
+
+export default function AstroInspect() {
+  const [currPanel, setCurrPanel] = useState('config')
+
+  useEffect(() => {
+    Emitter.on('load_table', () => {
+      setCurrPanel('table')
+    })
+
+    Emitter.on('back', () => {
+      setCurrPanel('config')
+    })
+  })
+
+  const BackButton = useCallback(() => {
+    return (
+      <>
+        <Button onClick={() => setCurrPanel('config')} className="d-inline-flex align-items-center me-2">
+          <FaArrowLeft size={14} className="me-2" />
+          <span>Back</span>
+        </Button>
+        <DownloadTableButton />
+      </>
+    )
+  }, [])
+
+  return (
+    <>
+      <div className="d-flex flex-column h-100">
+        <AppNavbar left={currPanel == 'table' ? <BackButton /> : ''} />
+        <div className="flex-grow-1">
+          <XTableDataProvider>
+            {currPanel == 'config' ? <ConfigForm /> : <XTableBody />}
+          </XTableDataProvider>
+        </div>
+      </div>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import { Container } from 'react-bootstrap'
-import AppNavbar from '@/components/common/AppNavbar'
 import LoadInitialState from '@/components/common/LoadInitialState'
-import ConfigForm from '@/components/setup/ConfigForm'
 import { XTableConfigProvider } from '@/contexts/XTableConfigContext'
-import XTableBody from '@/components/table/_XTableBody'
-import { XTableDataProvider } from '@/contexts/XTableDataContext'
 import Head from 'next/head'
+import AstroInspect from '@/components/common/AstroInspect'
 
 export default function XTable() {
   return (
@@ -17,17 +13,7 @@ export default function XTable() {
       <XTableConfigProvider>
         <LoadInitialState />
 
-        <AppNavbar />
-
-        <XTableDataProvider>
-          <Container className="mt-3">
-            <ConfigForm />
-          </Container>
-
-          <Container fluid className="px-0">
-            <XTableBody />
-          </Container>
-        </XTableDataProvider>
+        <AstroInspect />
       </XTableConfigProvider>
     </>
   )
