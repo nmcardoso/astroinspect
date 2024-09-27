6bee1166e0b82e77a1400cbe7123a54a30097388
Natanael <natanael_magalhaes@msn.com>
2024-09-27 02:32:41 -0300

    remove service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,19 @@ import { useEffect } from 'react'
 import { getBaseURL } from '../app/lib/utils'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    navigator.serviceWorker
-      .register(getBaseURL() + 'sw_v2.js', {
-        updateViaCache: 'none'
-      })
-      .then((registration) =>
-        console.log(
-          'Service Worker registration successful with scope: ',
-          registration.scope
-        )
-      )
-      .catch((err) => console.log('Service Worker registration failed: ', err))
-  }, [])
+  // useEffect(() => {
+  //   navigator.serviceWorker
+  //     .register(getBaseURL() + 'sw_v2.js', {
+  //       updateViaCache: 'none'
+  //     })
+  //     .then((registration) =>
+  //       console.log(
+  //         'Service Worker registration successful with scope: ',
+  //         registration.scope
+  //       )
+  //     )
+  //     .catch((err) => console.log('Service Worker registration failed: ', err))
+  // }, [])
 
   return <Component {...pageProps} />
 }
