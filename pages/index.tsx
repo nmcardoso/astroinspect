edd4a70d2fd6f1f8cb8ec33b6297c2b7804253e4
Natanael <natanael_magalhaes@msn.com>
2024-09-28 09:19:04 -0300

    add icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ export default function XTable() {
   return (
     <>
       <Head>
+        <link 
+          rel="icon" 
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌌</text></svg>" />
         <title>AstroInspect</title>
       </Head>
       
