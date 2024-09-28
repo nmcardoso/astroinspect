019c7a9d25bc631fb41c69f67190bd60dd131807
Natanael <natanael_magalhaes@msn.com>
2024-09-28 08:52:05 -0300

    analytics

---
+++ app/lib/gtag.ts
@@ -0,0 +1,1 @@
+export const GA_MEASUREMENT_ID = 'G-7YN2SWTKYY'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import { getBaseURL } from '../app/lib/utils'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { GoogleAnalytics, event, usePageViews } from 'nextjs-google-analytics'
+import { GA_MEASUREMENT_ID } from '../app/lib/gtag'
+
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  event(
+    name,
+    {
+      category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+      value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+      label: id, // id unique to current page load
+      nonInteraction: true, // avoids affecting bounce rate.
+      userId: GA_MEASUREMENT_ID
+    },
+  )
+}
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   // useEffect(() => {
@@ -18,8 +37,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   //     )
   //     .catch((err) => console.log('Service Worker registration failed: ', err))
   // }, [])
+  usePageViews({ gaMeasurementId: GA_MEASUREMENT_ID })
 
-  return <Component {...pageProps} />
+  return <>
+    <GoogleAnalytics gaMeasurementId={GA_MEASUREMENT_ID} />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
