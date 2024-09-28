6bb450ff37256f97bc3e5756e361ef4210c5925b
Natanael <natanael_magalhaes@msn.com>
2024-09-28 09:08:55 -0300

    new build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const basePath = process.env.NODE_ENV === 'production' ? '/astroinspect' : ''
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   typescript: {
