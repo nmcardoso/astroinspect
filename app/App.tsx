de3c26f1dac78e5af1b001c361b491c7f5c53948
Natanael <natanael_magalhaes@msn.com>
2024-10-24 01:10:49 -0300

    app routes

--- app/App.tsx
+++ app/App.tsx
@@ -1,22 +1,20 @@
-import { Button, Container } from 'react-bootstrap'
 import Appbar from '@/components/appbar/Appbar'
 import ConfigForm from '@/components/setup/ConfigForm'
 import AIGrid from '@/components/table/AIGrid'
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import Emitter from '@/lib/Emitter'
-import { FaArrowLeft } from "react-icons/fa";
-import DownloadTableButton from '@/components/appbar/DownloadTableButton'
 import { useXTableConfig } from './contexts/XTableConfigContext'
+import Plots from './components/plots/Plots'
 
 
 export default function App() {
-  const { tcState} = useXTableConfig()
+  const { tcState } = useXTableConfig()
   return (
     <>
       <div className="d-flex flex-column h-100">
         <Appbar />
         <div className="flex-grow-1">
-          {tcState.currentView == 'settings' ? <ConfigForm /> : <AIGrid />}
+          {tcState.currentView == 'settings' && <ConfigForm />}
+          {tcState.currentView == 'grid' && <AIGrid />}
+          {tcState.currentView == 'plots' && <Plots />}
         </div>
       </div>
     </>
