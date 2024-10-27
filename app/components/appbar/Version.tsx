5b0fb72b5e39d8a7393e3922456db7465a9ad2cf
Natanael <natanael_magalhaes@msn.com>
2024-10-27 01:01:19 -0300

    version

--- app/components/appbar/Version.tsx
+++ app/components/appbar/Version.tsx
@@ -1,9 +1,9 @@
-import packageJson from '@/../package.json'
+import {version} from '../../../package.json'
 
 export default function Version() {
   return (
     <span className="text-muded text-secondary fw-bold small">
-      v{packageJson.version}
+      v{version}
     </span>
   )
 }
