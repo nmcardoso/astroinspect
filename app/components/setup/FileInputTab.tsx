97904b3b18e226d5b819aec48855761bc2223d4b
Natanael <natanael_magalhaes@msn.com>
2024-12-20 23:58:26 -0300

    migrate FileInputTab to mui

--- app/components/setup/FileInputTab.tsx
+++ app/components/setup/FileInputTab.tsx
@@ -1,15 +1,28 @@
-import ButtonChip from '@/components/common/ButtonChip'
 import Help from '@/components/common/Help'
 import { useXTableConfig } from '@/contexts/XTableConfigContext'
 import { ContextActions } from '@/interfaces/contextActions'
-import { MouseEventHandler } from 'react'
-import Col from 'react-bootstrap/Col'
+import { MouseEventHandler, SyntheticEvent } from 'react'
 import Form from 'react-bootstrap/Form'
-import Row from 'react-bootstrap/Row'
 import { HiCheck, HiX } from 'react-icons/hi'
 import LocalFileInput from './LocalFileInput'
 import RemoteFileInput from './RemoteFileInput'
 import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from '@mui/material/FormControl'
+import FormLabel from '@mui/material/FormLabel'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import Stack from '@mui/material/Stack'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
+import StepContent from '@mui/material/StepContent'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
+import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import AIAutocomplete from '../common/AIAutocomplete'
 
 
 const StateMessage = ({ state }: { state: any }) => {
@@ -52,105 +65,94 @@ const StateMessage = ({ state }: { state: any }) => {
 function PositionColumns() {
   const { tcState, tcDispatch } = useXTableConfig()
 
-  const handleRAChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    const selectedIndex = parseInt(e.target.value)
-    let selectedCols = [...tcState.table.selectedColumnsId]
-    if (tcState.table.raIndex != undefined && selectedCols.includes(tcState.table.raIndex)) {
-      selectedCols = selectedCols.filter((e) => e != tcState.table.raIndex)
-    }
-    if (!selectedCols.includes(selectedIndex)) {
-      selectedCols.push(selectedIndex)
-    }
-    tcDispatch({
-      type: ContextActions.USER_FILE_INPUT,
-      payload: {
-        raIndex: selectedIndex,
-        raCol: tcState.table.columns?.[e.target.value as unknown as number],
-        selectedColumnsId: selectedCols,
-        state: selectedIndex >= 0 && tcState.table.decIndex != undefined && tcState.table.decIndex >= 0 ? 'success' : tcState.table.state
+  const handleRAChange = (e: SyntheticEvent<Element, Event>, newValue: any) => {
+    const selectedIndex = newValue?.index
+    if (selectedIndex == null) {
+      tcDispatch({
+        type: ContextActions.USER_FILE_INPUT,
+        payload: {
+          raIndex: -1,
+          raCol: undefined,
+          selectedColumnsId: tcState.table.selectedColumnsId,
+          state: tcState.table.state
+        }
+      })
+    } else {
+      let selectedCols = [...tcState.table.selectedColumnsId]
+      if (tcState.table.raIndex != undefined && selectedCols.includes(tcState.table.raIndex)) {
+        selectedCols = selectedCols.filter((e) => e != tcState.table.raIndex)
       }
-    })
+      if (!selectedCols.includes(selectedIndex)) {
+        selectedCols.push(selectedIndex)
+      }
+      tcDispatch({
+        type: ContextActions.USER_FILE_INPUT,
+        payload: {
+          raIndex: selectedIndex,
+          raCol: tcState.table.columns?.[newValue],
+          selectedColumnsId: selectedCols,
+          state: selectedIndex >= 0 && tcState.table.decIndex != undefined && tcState.table.decIndex >= 0 ? 'success' : tcState.table.state
+        }
+      })
+    }
   }
 
-  const handleDECChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    const selectedIndex = parseInt(e.target.value)
-    let selectedCols = [...tcState.table.selectedColumnsId]
-    if (tcState.table.decIndex != undefined && selectedCols.includes(tcState.table.decIndex)) {
-      selectedCols = selectedCols.filter((e) => e != tcState.table.decIndex)
-    }
-    if (!selectedCols.includes(selectedIndex)) {
-      selectedCols.push(selectedIndex)
-    }
-    tcDispatch({
-      type: ContextActions.USER_FILE_INPUT,
-      payload: {
-        decIndex: selectedIndex,
-        decCol: tcState.table.columns?.[e.target.value as unknown as number],
-        selectedColumnsId: selectedCols,
-        state: selectedIndex >= 0 && tcState.table.raIndex != undefined && tcState.table.raIndex >= 0 ? 'success' : tcState.table.state
+  const handleDECChange = (e: SyntheticEvent<Element, Event>, newValue: any) => {
+    const selectedIndex = newValue?.index
+    if (selectedIndex == null) {
+      tcDispatch({
+        type: ContextActions.USER_FILE_INPUT,
+        payload: {
+          decIndex: -1,
+          decCol: undefined,
+          selectedColumnsId: tcState.table.selectedColumnsId,
+          state: tcState.table.state
+        }
+      })
+    } else {
+      let selectedCols = [...tcState.table.selectedColumnsId]
+      if (tcState.table.decIndex != undefined && selectedCols.includes(tcState.table.decIndex)) {
+        selectedCols = selectedCols.filter((e) => e != tcState.table.decIndex)
       }
-    })
+      if (!selectedCols.includes(selectedIndex)) {
+        selectedCols.push(selectedIndex)
+      }
+      tcDispatch({
+        type: ContextActions.USER_FILE_INPUT,
+        payload: {
+          decIndex: selectedIndex,
+          decCol: tcState.table.columns?.[selectedIndex],
+          selectedColumnsId: selectedCols,
+          state: selectedIndex >= 0 && tcState.table.raIndex != undefined && tcState.table.raIndex >= 0 ? 'success' : tcState.table.state
+        }
+      })
+    }
   }
 
   return (
-    <Form.Group as={Row} className="mb-2" controlId="tableSourceSelector">
-      <Form.Label column sm="1" className="text-end">
-        { }
-      </Form.Label>
-      <Col sm={5}>
-        <InputGroup hasValidation>
-          <InputGroup.Text>RA</InputGroup.Text>
-          <Form.Select
-            required
-            isInvalid={tcState.table.raIndex == undefined || tcState.table.raIndex < 0}
-            isValid={tcState.table.raIndex !== undefined && tcState.table.raIndex >= 0}
-            value={tcState.table.raIndex as unknown as number}
-            onChange={handleRAChange}>
-            <option value={-1}>-</option>
-            {
-              tcState.table.columns.map((col, i) => (
-                <option value={i} key={col}>
-                  {col}
-                </option>
-              ))
-            }
-          </Form.Select>
-          <Form.Control.Feedback type="invalid">
-            RA column not found
-          </Form.Control.Feedback>
-          <Form.Control.Feedback type="valid">
-            RA column detected
-          </Form.Control.Feedback>
-        </InputGroup>
-      </Col>
+    <Stack direction="row" spacing={1} sx={{ alignItems: 'basline' }}>
+      <AIAutocomplete
+        label="RA"
+        isInvalid={tcState.table.raIndex == undefined || tcState.table.raIndex < 0}
+        isValid={tcState.table.raIndex !== undefined && tcState.table.raIndex >= 0}
+        value={{ index: tcState.table.raIndex, label: (tcState.table.raIndex != undefined && tcState.table.columns?.[tcState.table.raIndex]) || '' }}
+        onChange={handleRAChange}
+        invalidMessage="RA column not found"
+        getOptionLabel={opt => opt.label}
+        getOptionKey={opt => opt.index}
+        options={tcState.table.columns.map((c, i) => ({ label: c, index: i }))} />
 
-      <Col sm={5}>
-        <InputGroup hasValidation>
-          <InputGroup.Text>DEC</InputGroup.Text>
-          <Form.Select
-            required
-            isInvalid={tcState.table.decIndex == undefined || tcState.table.decIndex < 0}
-            isValid={tcState.table.decIndex !== undefined && tcState.table.decIndex >= 0}
-            value={tcState.table.decIndex as unknown as number}
-            onChange={handleDECChange}>
-            <option value={-1}>-</option>
-            {
-              tcState.table.columns.map((col, i) => (
-                <option value={i} key={col}>
-                  {col}
-                </option>
-              ))
-            }
-          </Form.Select>
-          <Form.Control.Feedback type="invalid">
-            DEC column not found
-          </Form.Control.Feedback>
-          <Form.Control.Feedback type="valid">
-            DEC column detected
-          </Form.Control.Feedback>
-        </InputGroup>
-      </Col>
-    </Form.Group>
+      <AIAutocomplete
+        label="DEC"
+        isInvalid={tcState.table.decIndex == undefined || tcState.table.decIndex < 0}
+        isValid={tcState.table.decIndex !== undefined && tcState.table.decIndex >= 0}
+        value={{ index: tcState.table.decIndex, label: (tcState.table.decIndex != undefined && tcState.table.columns?.[tcState.table.decIndex]) || '' }}
+        onChange={handleDECChange}
+        invalidMessage="DEC column not found"
+        getOptionLabel={opt => opt.label}
+        getOptionKey={opt => opt.index}
+        options={tcState.table.columns.map((c, i) => ({ label: c, index: i }))} />
+    </Stack>
   )
 }
 
@@ -166,54 +168,37 @@ const SourceSelector = () => {
   }
 
   return (
-    <Form.Group as={Row} className="mb-2" controlId="tableSourceSelector">
-      <Form.Label column sm="1" className="text-end">
-        Source
-      </Form.Label>
-      <Col sm="5">
-        <div className="d-flex align-items-center mt-2">
-          <Form.Check
-            inline
-            checked={tcState.table.type === 'local'}
-            label="local"
-            name="tableType"
-            type="radio"
-            value="local"
-            id="tableType-1"
-            onChange={handleTypeChange}
-          />
-          <Form.Check
-            inline
-            checked={tcState.table.type === 'remote'}
-            className="ms-2"
-            label="remote"
-            name="tableType"
-            type="radio"
-            value="remote"
-            id="tableType-2"
-            onChange={handleTypeChange}
-          />
-          <Help title="Upload Type">
-            Select upload type based on where the source table is stored.<br />
-            <ul>
-              <li>
-                <b>Local: </b> Table stored in local computer<br />
-              </li>
-              <li>
-                <b>Remote: </b> Table available in internet
-              </li>
-            </ul>
-          </Help>
-        </div>
-      </Col>
-    </Form.Group>
+    <Stack direction="row" sx={{ alignItems: 'center' }}>
+      <FormControl>
+        <RadioGroup
+          row
+          aria-labelledby="file-input-source-label"
+          name="file-input-source-group"
+          value={tcState.table.type}
+          onChange={handleTypeChange}>
+          <FormControlLabel value="local" control={<Radio />} label="Local" />
+          <FormControlLabel value="remote" control={<Radio />} label="Remote" sx={{ pl: 3 }} />
+        </RadioGroup>
+      </FormControl>
+      <Help title="Upload Type">
+        Select upload type based on where the source table is stored.<br />
+        <ul>
+          <li>
+            <b>Local: </b> Table stored in local computer<br />
+          </li>
+          <li>
+            <b>Remote: </b> Table available in internet
+          </li>
+        </ul>
+      </Help>
+    </Stack>
   )
 }
 
 
 function ColumnButton({ colName, colId }: { colName: string, colId: number }) {
   const { tcState, tcDispatch } = useXTableConfig()
-  const cls = tcState.table.selectedColumnsId.includes(colId) ? 'btn-primary' : 'btn-outline-primary'
+  const variant = tcState.table.selectedColumnsId.includes(colId) ? 'filled' : 'outlined'
 
   const handleToggle: MouseEventHandler<HTMLDivElement> = () => {
     let newColumns = []
@@ -234,11 +219,12 @@ function ColumnButton({ colName, colId }: { colName: string, colId: number }) {
     })
   }
   return (
-    <ButtonChip
-      className={`${cls} me-2 mt-1`}
-      onClick={handleToggle}>
-      {colName}
-    </ButtonChip>
+    <Chip
+      variant={variant}
+      color="primary"
+      sx={{ mr: 1, mt: 1 }}
+      onClick={handleToggle}
+      label={colName} />
   )
 }
 
@@ -250,24 +236,16 @@ function FileInputColumns() {
     <>
       {
         tcState.table.columns && tcState.table.columns.length > 0 && (
-          <Form.Group as={Row} className="mb-2" controlId="tableSourceSelector">
-            <Form.Label column sm="1" className="text-end">
-              Columns
-            </Form.Label>
-            <Col sm="11">
-              <div style={{ maxHeight: 250, overflow: 'auto' }}>
-                {
-                  tcState.table.columns.map(((col, i) => (
-                    <ColumnButton key={i} colId={i} colName={col} />
-                  )))
-                }
-                <Help title="Add Columns">
-                  Select columns from input table to include.
-                </Help>
-              </div>
-
-            </Col>
-          </Form.Group>
+          <Box sx={{ maxWidth: '100%', maxHeight: 250, overflow: 'auto' }}>
+            {
+              tcState.table.columns.map(((col, i) => (
+                <ColumnButton key={i} colId={i} colName={col} />
+              )))
+            }
+            {/* <Help title="Add Columns">
+              Select columns from input table to include.
+            </Help> */}
+          </Box>
         )
       }
     </>
@@ -280,20 +258,50 @@ export default function FileInputTab() {
 
   return (
     <>
-      <SourceSelector />
+      <Stepper activeStep={0} orientation="vertical" nonLinear={true}>
+        <Step expanded={true} active={true}>
+          <StepLabel optional={undefined}>
+            Source type
+          </StepLabel>
+          <StepContent TransitionProps={{ unmountOnExit: false }}>
+            <SourceSelector />
+          </StepContent>
+        </Step>
 
-      <div className={tcState.table.type == 'remote' ? 'd-none' : ''}>
-        <LocalFileInput />
-      </div>
-      <div className={tcState.table.type == 'local' ? 'd-none' : ''}>
-        <RemoteFileInput />
-      </div>
+        <Step expanded={true} active={true}>
+          <StepLabel optional={undefined}>
+            Upload file
+          </StepLabel>
+          <StepContent TransitionProps={{ unmountOnExit: false }}>
+            <div className={tcState.table.type == 'remote' ? 'd-none' : ''}>
+              <LocalFileInput />
+            </div>
+            <div className={tcState.table.type == 'local' ? 'd-none' : ''}>
+              <RemoteFileInput />
+            </div>
+          </StepContent>
+        </Step>
 
-      <div className={tcState.table.state == 'unloaded' ? 'd-none' : ''}>
-        <PositionColumns />
-      </div>
+        <Step expanded={true} active={tcState.table.state != 'unloaded'}>
+          <StepLabel optional={undefined}>
+            Configure coordinates
+          </StepLabel>
+          <StepContent TransitionProps={{ unmountOnExit: false }}>
+            <div className={tcState.table.state == 'unloaded' ? 'd-none' : ''}>
+              <PositionColumns />
+            </div>
+          </StepContent>
+        </Step>
 
-      <FileInputColumns />
+        <Step expanded={true} active={tcState.table.columns && tcState.table.columns.length > 0}>
+          <StepLabel optional={undefined}>
+            Select columns
+          </StepLabel>
+          <StepContent TransitionProps={{ unmountOnExit: false }}>
+            <FileInputColumns />
+          </StepContent>
+        </Step>
+      </Stepper>
     </>
   )
 }
