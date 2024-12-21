f7a0f0a1f0f91e591df02c0fd354e8e426b2f3b2
Natanael <natanael_magalhaes@msn.com>
2024-12-20 23:49:16 -0300

    static pages generation: create folders instead of files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const basePath = process.env.NODE_ENV === 'production' ? '' : ''
 
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
   typescript: {
