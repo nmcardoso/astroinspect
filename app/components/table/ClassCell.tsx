a18cc79cf6bdfdf5aa79568e51900b7db576d22e
Natanael <natanael_magalhaes@msn.com>
2024-12-27 03:07:59 -0300

    migrate class cell to mui

--- app/components/table/ClassCell.tsx
+++ app/components/table/ClassCell.tsx
@@ -1,22 +1,31 @@
-import Form from 'react-bootstrap/Form'
 import { useXTableConfig } from '@/contexts/XTableConfigContext'
-import { CustomCellRendererProps } from '@ag-grid-community/react'
+import type { GroupCellRendererParams } from 'ag-grid-community'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
 
 
-export default function ClassCell(params: CustomCellRendererProps) {
+export default function ClassCell(params: GroupCellRendererParams) {
   const { tcState } = useXTableConfig()
 
   return (
-    <Form.Select
-      value={params.value}
-      onChange={e => {
-        params.api.getRowNode(params.data['ai:id'])?.setDataValue('ai:class', e.target.value)
-        // console.log(params.api.getRowNode(params.data._id).data)
-      }}>
-      <option value="">-</option>
-      {tcState.cols.classification.classNames.map(cls => (
-        <option key={cls} value={cls}>{cls}</option>
-      ))}
-    </Form.Select>
+    <FormControl variant="standard" fullWidth>
+      <InputLabel id={`class-select-label-${params.data['ai:id']}`}>class</InputLabel>
+      <Select
+        labelId={`class-select-label-${params.data['ai:id']}`}
+        id={`class-select-${params.data['ai:id']}`}
+        value={params.value || ''}
+        label="class"
+        onChange={e => {
+          params.api.getRowNode(params.data['ai:id'])?.setDataValue('ai:class', e.target.value)
+          // console.log(params.api.getRowNode(params.data._id).data)
+        }}>
+        <MenuItem value="">-</MenuItem>
+        {tcState.cols.classification.classNames.map(cls => (
+          <MenuItem key={cls} value={cls}>{cls}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   )
 }
