e055ce4542de35dc9bfd8fef5596a081bbe82b84
Natanael <natanael_magalhaes@msn.com>
2025-03-02 06:29:51 -0300

    fix legacy stamps

--- app/services/legacy.ts
+++ app/services/legacy.ts
@@ -6,9 +6,9 @@ import { SimpleConeSearchClient } from './scs'
 import interp1 from 'interp1'
 import { processResponse } from './utils'
 
-const LEGACY_RGB = 'https://www.legacysurvey.org/viewer/cutout.jpg'
+const LEGACY_RGB = 'https://bittersweet-large-ticket.glitch.me/https://www.legacysurvey.org/viewer/cutout.jpg'
 // const LEGACY_RGB = 'https://checker-melted-forsythia.glitch.me/legacy.jpg'
-const LEGACY_RGB_2 = 'http://alasky.cds.unistra.fr/hips-image-services/hips2fits'
+const LEGACY_RGB_2 = 'https://alasky.cds.unistra.fr/hips-image-services/hips2fits'
 const SCS_URL = 'https://bittersweet-large-ticket.glitch.me/https://datalab.noirlab.edu/scs/ls_dr10/tractor'
 const MAG_R = [0, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 100]
 const CF_CIRC_LINEAR = [11.50, 11.50, 6.00, 5.90, 5.30, 4.50, 3.95, 3.45, 4.20, 4.80, 7.10, 5.60, 1.50, 1.50]
