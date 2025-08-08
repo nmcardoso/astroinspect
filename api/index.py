f59e78a85e2dc6865619e3ad65b89eb1a76e256d
Natanael <natanael_magalhaes@msn.com>
2025-08-08 05:47:48 -0300

    cache max_age

--- api/index.py
+++ api/index.py
@@ -61,7 +61,9 @@ def get_token():
 def include_cache_control(headers):
   excluded_headers = ['cache-control', 'age']
   headers = [(name, value) for name, value in headers if name.lower() not in excluded_headers]
-  headers.append(('cache-control', 'public,max-age=604800,immutable'))
+  headers.append(('cache-control', 'public,max-age=3600'))
+  headers.append(('Vercel-CDN-Cache-Control', 'max-age=3600'))
+  headers.append(('CDN-Cache-Control', 'max-age=60'))
   headers.append(('age', '0'))
   return headers
 
