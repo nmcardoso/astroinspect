ae4da2927e64b943fe13f689b23f28d5cda3b812
Natanael <natanael_magalhaes@msn.com>
2025-09-11 09:42:59 -0300

    headers

--- api/index.py
+++ api/index.py
@@ -104,8 +104,9 @@ def proxy(path):
       'access-control-allow-origin', 'access-control-allow-headers',
       'access-control-allow-methods',
     ]
+    print(resp.raw.headers.items())
     headers = [
-      (name, value) for name, value in resp.headers.items() 
+      (name, value) for name, value in resp.raw.headers.items()
       if name.lower() not in excluded_headers
     ]
     # headers = include_cache_control(headers)
